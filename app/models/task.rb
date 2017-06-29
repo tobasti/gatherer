8e6a15929a81822893a3b33e59c2142c8b7f7bea
Tobasti <tobasti@riseup.net>
2017-06-29 15:59:29 +0200

    enable velocity calculations in Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,14 +1,25 @@
 class Task
+  attr_accessor :size, :completed_at
 
-  def initialize
-    @completed = false
+  def initialize(options = {})
+    mark_completed(options[:completed_at]) if options[:completed_at]
+    @size = options[:size]
   end
 
   def complete?
-    @completed
+    @completed_at.present?
   end
 
-  def mark_completed
-    @completed = true
+  def mark_completed(date = nil)
+    @completed_at = (date || Time.current)
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > 3.weeks.ago
+  end
+
+  def points_towards_velocity
+    part_of_velocity? ? @size : 0
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe Task do
     task.mark_completed
     expect(task).to be_complete
   end
+
+  describe "velocity" do
+    let(:task) { Task.new(size: 3) }
+
+    it "does not count an incomplete task towards velocity" do
+      expect(task).to_not be_part_of_velocity
+      expect(task.points_towards_velocity).to eq(0)
+    end
+
+    it "does not count a long-ago completed task towards velocity" do
+      task.mark_completed(6.months.ago)
+      expect(task).to_not be_part_of_velocity
+      expect(task.points_towards_velocity).to eq(0)
+    end
+
+    it "counts a recently completed task towards velocity" do
+      task.mark_completed(1.day.ago)
+      expect(task).to be_part_of_velocity
+      expect(task.points_towards_velocity).to eq(3)
+    end
+  end
 end
